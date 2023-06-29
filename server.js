d9fe918383ace5d5b7fe6cbf02c68eac98615a17
Aashi <ashi471204@gmail.com>
2023-06-29 18:37:02 +0530

    commit

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use('/details', details)
   
 
 
-//Server is listening to port number
+//Server is listening to port numbers
 app.listen(1000, () => {
   console.log("Server is running on port 1000");
 });
